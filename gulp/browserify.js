40cd80a1c9c6e9927ecffb8bf5cfb6ca8f32c97d
Scott Taylor <scott.c.taylor@mac.com>
2015-08-12 17:33:09 -0400

    New GULP stuff

---
+++ gulp/browserify.js
@@ -0,0 +1,48 @@
+var gulp = require( 'gulp' ),
+	watchify = require( 'watchify' ),
+	browserify = require( 'browserify' ),
+	source = require( 'vinyl-source-stream' ),
+	buffer = require( 'vinyl-buffer' ),
+	gutil = require( 'gulp-util' ),
+	assign = require( 'lodash.assign' ),
+	uglify = require( 'gulp-uglify' ),
+	rename = require( 'gulp-rename' ),
+	opts,
+	b;
+
+opts = assign( {}, watchify.args, customOpts = {
+	entries: [ './js/main.js' ]
+} );
+
+gutil.log( 'Starting watchify...' );
+b = watchify( browserify( opts ) );
+
+b.on( 'update', bundle );
+b.on( 'log', gutil.log );
+b.on( 'error', gutil.log.bind( gutil, 'Browserify Error' ) );
+
+function bundle() {
+	var DEST = './build/js';
+
+	gutil.log( 'Browserifying files...' );
+
+	return b.bundle()
+		.pipe( source( 'compiled.js' ) )
+		.on( 'end', function () {
+			gutil.log( 'Saved to ./build/js' );
+			gutil.log( 'Uglifying files...' );
+		})
+		.pipe( gulp.dest( DEST ) )
+		.pipe( buffer() )
+		.pipe( uglify() )
+		.on( 'end', function () {
+			gutil.log( 'Saving minified files to ./build/js' );
+		})
+		.pipe( rename({ extname: '.min.js' }) )
+		.pipe( gulp.dest( DEST ) )
+		.on( 'end', function () {
+			gutil.log( 'DONE.' );
+		});
+}
+
+module.exports = bundle;
--- gulp/js.js
+++ gulp/js.js
@@ -1,34 +1,19 @@
 var gulp = require( 'gulp' ),
-	rename = require( 'gulp-rename' ),
-	source = require( 'vinyl-source-stream' ),
-	streamify = require( 'gulp-streamify' ),
-	uglify = require( 'gulp-uglify' ),
-
+	gutil = require( 'gulp-util' ),
 	jshint = require( 'gulp-jshint' ),
-	browserify = require( 'browserify' );
+	bundle = require( './browserify' );
 
 module.exports = function () {
-	var DEST = './build/js',
-		bundleStream;
+	gutil.log( 'JSLint\'ing files...' );
 
 	gulp.src( [
 			'js/**/*.js',
-			'!js/lib/**/*.js',
 			'!js/gulp/**/*.js',
 			'!js/templates/**/*.js'
 		] )
 		.pipe( jshint() )
-		.pipe( jshint.reporter( 'jshint-stylish' ) )
+		.pipe( jshint.reporter( 'jshint-stylish', { verbose: true } ) )
 		.pipe( jshint.reporter( 'fail' ) );
 
-	bundleStream = browserify( [
-		'./js/main.js'
-	] ).bundle();
-
-	return bundleStream
-		.pipe( source( 'compiled.js' ) )
-		.pipe( gulp.dest( DEST ) )
-		.pipe( streamify( uglify() ) )
-		.pipe( rename({ extname: '.min.js' }) )
-		.pipe( gulp.dest( DEST ) );
+	bundle();
 };
