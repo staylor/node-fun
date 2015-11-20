687371713aa7d17b6f6dedff6d3ce0dc8e01f3ea
Scott Taylor <scott.c.taylor@mac.com>
2015-11-20 18:07:15 -0500

    this works for now

---
+++ js/controllers/songkick.js
@@ -0,0 +1,30 @@
+var Songkick = require( '../providers/songkick' ),
+	SongkickModel = require( '../models/songkick' ),
+	SongickCollection = require( '../collections/songkick' );
+
+module.exports = function ( req, res ) {
+	var s = new Songkick();
+
+//	s.getMetroEvents( Songkick.NYC_METRO ).then( function ( shows ) {
+//		var SongkickShows,
+//			parsed;
+
+//		parsed = shows.map( function ( show ) {
+//			return SongkickModel.parseData( show );
+//		} );
+//
+//		SongkickShows = new SongickCollection( parsed );
+
+		res.locals = {
+			shows: []
+		};
+		res.render( 'index', {
+			partials: {
+				show: 'show'
+			}
+		} );
+//	}, function ( err ) {
+//		console.error( err.stack );
+//		res.status( 500 ).send( 'Something broke!' );
+//	} );
+};
